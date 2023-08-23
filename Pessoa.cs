2dfa289473789b2591c8203fd069f90e5ecd398e
MoizesFerreir <moizesrafael@oultook.com>
2023-08-22 21:52:52 -0300

    treino

---
+++ Pessoa.cs
@@ -0,0 +1,42 @@
+ using System;
+ using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ namespace pooCalculaIdade
+ {
+ public class Pessoa
+    {
+        private int anoNascimento;
+
+        public int AnoNascimento
+        {
+            get { return this.anoNascimento; }
+            set { anoNascimento = value; }
+        }
+
+        private String nome;
+        public String Nome
+        {
+            get { return nome; }
+            set { nome = value.ToUpper();}
+        }
+        
+        public void ExibirDados()
+        {
+            Console.WriteLine("Nome" + this.Nome);
+            Console.WriteLine("Ano de Nascimento: " + this.AnoNascimento);
+            int idade = this.CalcularIdade();
+            Console.WriteLine("Idade: "+idade);
+        }
+
+        private int CalcularIdade()
+        {
+            DateTime data = DateTime.Now;
+            int ano = data.Year;
+            int idade = ano - this.AnoNascimento;
+            return idade;
+        }
+    }
+ }
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooCalculaIdade
+{
+   class Program
+   {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Programa que calcula a Idade de uma pessoa");
+        Pessoa p = new Pessoa();
+        Console.Write("Nome da pessoa: ");
+        p.Nome = Console.ReadLine();
+        Console.Write("Ano de nascimento: ");
+        p.AnoNascimento = Convert.ToInt32(Console.ReadLine());
+        p.ExibirDados();
+    }
+   }
+}
+
+
